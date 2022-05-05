3b0d5b3911c34b678a88a13af124eb094056d04b
geoffery0 <geoffery@DESKTOP-9U7JLCN.localdomain>
2022-05-04 23:59:40 -0700

    refactored

--- src/lab/assignment/beforerefactoring/CoffeeMaker.java
+++ src/lab/assignment/beforerefactoring/CoffeeMaker.java
@@ -1,22 +1,15 @@
 package lab.assignment.beforerefactoring;
 
-public class CoffeeMaker {
+public abstract class CoffeeMaker{
 
-    public void prepareMochaCoffee(){
+    public final void prepareCoffee(){
         boilWater();
         brewEspresso();
-        addIngredientsForMocha();
-        finalTouchForMocha();
+        addIngredients();
+        finalTouch();
         System.out.println(">>Serving coffee");
     }
 
-    public void prepareCapuccinoCoffee(){
-        boilWater();
-        brewEspresso();
-        addIngredientsForCapuccino();
-        finalTouchForCapuccino();
-        System.out.println(">>Serving coffee");
-    }
 
     private void boilWater() {
         System.out.println("Boiling water");
@@ -26,22 +19,10 @@ public class CoffeeMaker {
         System.out.println("Brewing espresso");
     }
 
-    private void addIngredientsForMocha(){
-        System.out.println("Adding cocoa powder");
-        System.out.println("Adding hot milk");
-    }
+    protected abstract void addIngredients();
+    protected abstract void finalTouch();
 
-    private void addIngredientsForCapuccino(){
-        System.out.println("Add hot milk");
-        System.out.println("Add cinnamon");
-    }
-
-    private void finalTouchForMocha(){
-        System.out.println("Adding sugar");
-    }
-
-    private void finalTouchForCapuccino(){
-        System.out.println("Top with milk foam");
+    
     }
 
 
